050c2554d57864a0a00956d58be17ef0f9478d14
Rene Molina <rdmolina@miners.utep.edu>
2022-02-20 18:28:30 -0700

    Project 0 Complete
    Makefile updated for 8x12 and 5x7 fonts, draw.h includes arrow funtion and draw_chars functions for 8x12 and 5x7 fonts, draw_shapes is updated with arrow function, draw_chars is updated with functions for 8x12 and 5x7 fonts, and the user interface is updated to handle these changes

--- uimain.c
+++ uimain.c
@@ -7,13 +7,13 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c, Arrow = a) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
     if (c == EOF)		     /* terminate on end-of-file */
       goto done;
-
+    
     // Given the user answer, select which method to call
     switch (c) {
     case 't':
@@ -25,9 +25,56 @@ int main()
       print_square(5, 5);
       break;
     case 'c':
-      puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
-	print_char_11x16(c);
+      fputs("Select what font you would like (11x16 = l, 8x12 = m, 5x7 = s)\n", stdout);
+      fflush(stdout);
+      while ((c = getchar()) == '\n'); /* ignore newlines */
+      if (c == EOF)
+	goto done;
+      else if (c == 'l') {
+	int startC;
+        puts("You selected chars:");
+        fputs("Select the starting character for the sequence\n", stdout);
+        fflush(stdout);
+        while ((startC = getchar()) == '\n'); /* ignore newlines */
+        if (startC > (0x7E - 0x02)) {
+	  printf("Out of range, please try again!\n");
+	  break;
+	  }
+	for (char c = startC; c < startC+3; c++)
+	  print_char_11x16(c);
+	}
+      else if (c == 'm') {
+	int startC;
+        puts("You selected chars:");
+        fputs("Select the starting character for the sequence\n", stdout);
+        fflush(stdout);
+        while ((startC = getchar()) == '\n'); /* ignore newlines */
+        if (startC > (0x7E - 0x02)) {
+	  printf("Out of range, please try again!\n");
+	  break;
+	  }
+	for (char c = startC; c < startC+3; c++)
+	  print_char_8x12(c);
+        }
+      else if (c == 's') {
+	int startC;
+        puts("You selected chars:");
+        fputs("Select the starting character for the sequence\n", stdout);
+        fflush(stdout);
+        while ((startC = getchar()) == '\n'); /* ignore newlines */
+        if (startC > (0x7E - 0x06)) {
+	  printf("Out of range, please try again!\n");
+	  break;
+	  }
+	for (char c = startC; c < startC+7; c++)
+	  print_char_5x7(c);
+        }
+      else
+	printf("Unrecognized option '%c', please try again!\n", c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
+      print_arrow(5,7);
       break;
     case 'q':
       puts("Bye!");
